1289b4a0107ebb48e9a3d66ec61cabb97ff70ae2
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2026-08-09 13:06:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = int(os.environ.get("API_ID", "24222039"))
     API_HASH = os.environ.get("API_HASH", "6dd2dc70434b2f577f76a2e993135662")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7905740502:AAGzYK-V4HfxjJ4IYXsk_RPv6cc8rnAbAR0")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7905740502:AAEbesPWE30nXZwcAibymVfvi4gZVvJTyuI")
     
     # Dual Database Active Mode URIs (DB1 & DB2)
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://rajibchaun_db_user:Fa1SV672h6xrxFxR@cluster0.pfgtal2.mongodb.net/?appName=Cluster0")
